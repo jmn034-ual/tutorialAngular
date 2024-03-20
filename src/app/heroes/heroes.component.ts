7fc11f677d94d70baf9d50b7d59a156e1c64f9fb
jmn034 <jmn034@inlumine.ual.es>
2024-03-20 21:51:30 +0100

    Segundo commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {HEROES} from '../mock-heroes';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [UpperCasePipe, FormsModule, NgFor, NgIf],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+}
