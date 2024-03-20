648b92b499a00a60ffd790aa3b79528ac3eed4c4
jmn034 <jmn034@inlumine.ual.es>
2024-03-20 22:42:40 +0100

    Hasta aqui funciona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroesComponent,],
+  imports: [HeroesComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
-import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {Component} from '@angular/core';
+import {
+  NgIf,
+  NgFor,
+  UpperCasePipe,
+} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
-import { Hero } from '../hero';
 
 @Component({
-  selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule, NgFor, NgIf],
+  selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrls: ['./heroes.component.css'],
+  imports: [
+    FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,
+  ],
 })
+
 export class HeroesComponent {
   heroes = HEROES;
   selectedHero?: Hero;
+
   onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+    this.selectedHero = hero;
+  }
 }
