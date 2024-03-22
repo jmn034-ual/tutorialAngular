95a25a42cae67863ad61abac02ce34cec824ce59
jmn034 <jmn034@inlumine.ual.es>
2024-03-22 21:05:42 +0100

    Tutorial finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
-
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroesComponent, MessagesComponent],
+  imports: [HeroesComponent, MessagesComponent, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,7 @@ import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
@@ -24,14 +24,14 @@ import { MessageService } from '../message.service';
     NgFor,
     UpperCasePipe,
     HeroDetailComponent, 
+    RouterLink
     ],
 })
 
 export class HeroesComponent implements OnInit{
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
  
   getHeroes(): void {
     this.heroService.getHeroes()
@@ -40,10 +40,5 @@ export class HeroesComponent implements OnInit{
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
